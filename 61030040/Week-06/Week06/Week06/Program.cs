130c59e9efa88c5f1e4fcfb7a469a77f1972f156
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2019-12-18 01:47:28 +0700

    Exam 1-5

--- 61030040/Week-06/Week06/Week06/Program.cs
+++ 61030040/Week-06/Week06/Week06/Program.cs
@@ -1,35 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Week06
+class Program
 {
-   
-    class Program
+    static void Main()
     {
-        static void Main()
-        {
-            Console.WriteLine("Divide seven");
-               Console.Write("Enter number Begin: ");
-            int b = int.Parse(Console.ReadLine());
-            Console.Write("Enter number last: ");
-            int l = int.Parse(Console.ReadLine());
+        Console.Write("Enter mass in earth: ");
+        /*
+        การชั่งน้ำหนักจากเครื่องชั่งน้ำหนักนั้นเป็นการชั่งมวล ไม่ใช่น้ำหนักจริงๆบนโลก
+        การหาน้ำหนักบนโลก จะเป็น W = mg ตามกฎของนิวตัน
+        W คือน้ำหนัก(N)
+        m คือมวล(kg)
+        g คือแรงดึงดูดของโลก มีค่า = 9.81 m/s^2
+        */
+        float m = float.Parse(Console.ReadLine());
 
-            finddiv7(b, l);
-            Console.ReadKey();
-        }
-        static void finddiv7(int b, int l)
-        {
-            for (int i = b; i <= l; i++)
-            {
-                if (i % 7 == 0)
-                    Console.Write($"{i} ");
-            }
-        }
+        weigth_Moon(m);
+        Console.ReadKey();
+    }
 
-
-
+    static void weigth_Moon(float mass)
+    {
+        //ค่า g บนโลกจะมากกว่าบนดวงจันทร์ 6 เท่า
+        Console.WriteLine("Weigth in moon is {0:F1}", mass * (9.8 / 6));
     }
 }
